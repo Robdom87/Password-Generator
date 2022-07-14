e9b67feecd4c91f6fc17f6e029e286c6df97b7b9
Cedar Chavez <geggas00@gmail.com>
2022-07-14 00:53:50 -0700

    conceptual framework added to script

--- Develop/script.js
+++ Develop/script.js
@@ -1,14 +1,40 @@
 // Assignment Code
-var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate"); //has the whole generate button html in there
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var password = generatePassword(); //in the write password function genrate password function is run and value stored in password
+  var passwordText = document.querySelector("#password"); //selected text area and stored in passwordText
 
-  passwordText.value = password;
+  passwordText.value = password; //assigned password to passwordText.value, and should replace placeholder value in box
+  
 
 }
 
+function generatePassword() {
+  var length = passwordLength ();
+  var types = passwordCharTypes ();
+// include recursive function to the length of the password
+//random number generator to figure out the character to for each bit of the string
+  return ; //return the full password
+}
+
+function passwordLength(){
+  //creates prompt where password length is asked for
+  //validate if between 8 - 128
+  //if failed, reprompt with mention on the criteria
+  //return password length when validated
+}
+
+function passwordCharTypes(){
+  //send confirm box for lowercase, uppercase, numeric, special characters
+  //mention that atleast one will be selected
+  //and click cancel to exclude the character type
+  //confirm that atleast one was selected
+  //if not add prompt, to see if they want to try again
+  //return booleans for true or false of each character type
+  return;
+}
+
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword); //once generate button is clickec run the function write password
