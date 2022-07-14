4ed20532ac7fe46f662477360c298b6f0b8a1d90
Cedar Chavez <geggas00@gmail.com>
2022-07-14 16:32:26 -0700

    added new character function to create a new chracter of any character type and debugging

--- Develop/script.js
+++ Develop/script.js
@@ -7,61 +7,107 @@ function writePassword() {
   var passwordText = document.querySelector("#password"); //selected text area and stored in passwordText
 
   passwordText.value = password; //assigned password to passwordText.value, and should replace placeholder value in box
-  
+
 
 }
 
 function generatePassword() {
-  let length = passwordLength ();
+  let length = passwordLength();
+  let type = passwordCharTypes();
   let passString = "";
   var newChar = "";
 
   // forloop to the length of the password
   for (let i = 0; i < length; i++) {
-    newChar = newCharacter ();
+    newChar = newCharacter(type);
     passString = passString + newChar;
   }
 
   return passString; //return the full password
 }
 
-function newCharacter(){
-  let types = passwordCharTypes ();
+
+
+function newCharacter(type) {
+  var nuevo = "";
+
+  //add types across to see
+  let u = false;
+  let l = false;
+  let n = false;
+  let s = false;
+
+  //convert 0 and 1's back to booleans
+  if (type[0] === "1") {
+    u = true;
+  } else {}
   
+  if (type[1] === "1") {
+    l = true;
+  } else {}
+  
+  if (type[2] === "1") {
+    n = true;
+  } else {}
+  
+  if (type[3] === "1") {
+    s = true;
+  }
 
-  //random number generator to figure out the character to for each bit of the string
-
+  //random number generator to figure out which character type to get for each character in the password
+  while (nuevo === "") {
+    let random = Math.floor(Math.random() * 4);
+    if (random === 0 && u === true) {
+      nuevo = upperCharacter();
+    } else if (random === 1 && l === true) {
+      nuevo = lowerCharacter();
+    } else if (random === 2 && n === true) {
+      nuevo = numericCharacter();
+    } else if (random === 3 && s === true) {
+      nuevo = specialCharacter();
+    }
+  }
+  console.log(nuevo);
+  return nuevo;
 }
 
+//outputs random uppercase character
 function upperCharacter() {
-
+  let uL = "a";
+  return uL;
 }
 
+//outputs random lowercase character
 function lowerCharacter() {
-  
+  let lL = "b";
+  return lL;
 }
 
+//outputs random numeric character
 function numericCharacter() {
-  
+  let nC = "c";
+  return nC;
 }
 
+//outputs random special character
 function specialCharacter() {
-  
+  let sC = "d";
+  return sC;
 }
 
-function passwordLength(){
+function passwordLength() {
   //creates prompt where password length is asked for
   let inputLen = prompt("Input desired password length.\nMust be at least 8 characters long. and no more than 128 characters long.", "8");
   //validate if between 8 - 128, reprompt until meets criteria
   while (inputLen < 8 || inputLen > 128) {
     inputLen = prompt("Input desired password length.\nMust be at least 8 characters long. and no more than 128 characters long.", "8");
-  } 
+  }
   //return password length when validated
   return inputLen;
-  
+
 }
 
-function passwordCharTypes(){
+function passwordCharTypes() {
   //send alert that atleast one should be selected.
   alert("Next, we will select the character types to include in the password.\nNOTICE: atleast one character type should be selected.");
   //send confirm box for lowercase, uppercase, numeric, special characters
@@ -80,37 +126,37 @@ function passwordCharTypes(){
     numeric = confirm("Include NUMERIC characters in password?\nClick cancel to exclude character type.");
     special = confirm("Include SPECIAL characters in password?\nClick cancel to exclude character type.");
   }
-  
+
   //return booleans for true or false of each character type, convert to 0 and 1's
   let aggTypes = "";
-  if (upper=== true){
+  if (upper === true) {
     aggTypes = aggTypes + "1";
   }
   else {
     aggTypes = aggTypes + "0";
   }
 
-  if (lower=== true){
+  if (lower === true) {
     aggTypes = aggTypes + "1";
   }
   else {
     aggTypes = aggTypes + "0";
   }
 
-  if (numeric=== true){
+  if (numeric === true) {
     aggTypes = aggTypes + "1";
   }
   else {
     aggTypes = aggTypes + "0";
   }
 
-  if (special=== true){
+  if (special === true) {
     aggTypes = aggTypes + "1";
   }
   else {
     aggTypes = aggTypes + "0";
   }
-  
+
   return aggTypes;
 }
 
